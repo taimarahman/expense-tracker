1d228374c6d6fb29de8f83644f7cb78178487281
Taima Rahman <taimarh@gmail.com>
2023-12-01 00:54:10 +0600

    material setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'expense-tracker';
+  color: ThemePalette = 'accent';
+  checked = false;
+  disabled = false;
 }
