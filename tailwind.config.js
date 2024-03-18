629d3ce3c17b891eef339eca2f419cb824930291
Taima Rahman <taimarh@gmail.com>
2024-03-18 23:09:54 +0600

    fix styles and css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts,scss}",
+    "./src/**/*.{html,ts}",
   ],
   theme: {
     fontFamily: {
       'alumni': ['Alumni Sans', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      colors:{
+        'primary': {
+          600: '#4b7e93',
+          300: '#a2c8db',
+          200: '#e3eaef',
+          100: '#f2f7fd',
+        },
+        'secondary': {
+          600: '#fc834a',
+          300: '#bf7958',
+        },
+        'dark' : '#0b0b23',
+      },
+    },
   },
   plugins: [],
 }
